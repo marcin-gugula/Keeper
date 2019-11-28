e4a39c8ef0e28d67752feb0df4f7d6b2b0590d01
marcin.gugula <recble@gmail.com>
2019-11-28 22:22:31 +0100

    Add more notes with map

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import Heading from "./Heading";
 import Note from "./Note";
 import Footer from "./Footer";
+import notes from "../notes"
 
 function App() {
   return (
     <div>
       <Heading />
-      <Note />
+      {notes.map( props => (<Note
+        key={props.key}
+      title={props.title}
+      content={props.content}
+      />))}
       <Footer />
     </div>
   );
